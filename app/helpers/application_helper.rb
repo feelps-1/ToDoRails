46bdd57ca4034fb188764f1acfeb5e7f35d5876c
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2025-01-12 15:21:03 -0300

    fix: status color not working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def tailwind_status_style(status)
     case status
-    when "to_do"
+    when "a_fazer"
       "bg-yellow-100 text-yellow-700"
-    when "in_progress"
+    when "em_andamento"
       "bg-blue-100 text-blue-700"
-    when "done"
+    when "terminado"
       "bg-green-100 text-green-700"
     else
       "bg-gray-100 text-gray-700"
